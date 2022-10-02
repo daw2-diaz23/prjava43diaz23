4897032d5e3fd4d1a89e7ffbc49de76007e63c9e
daw2-diaz23 <diazcalabuigpau@fpllefia.com>
2022-10-02 16:53:44 +0200

    Versió 0.4-B del projecte prjava43diaz23. Branca master modificada.

--- src/prjava43diaz23/Prjava43diaz23.java
+++ src/prjava43diaz23/Prjava43diaz23.java
@@ -1,6 +1,8 @@
 package prjava43diaz23; 
+
 import java.awt.*;        // using AWT containers and components 
 import java.awt.event.*;  // using AWT events and listener interfaces 
+import java.net.*;
  
 // An AWT GUI program inherits the top-level container java.awt.Frame 
 public class Prjava43diaz23 extends Frame 
